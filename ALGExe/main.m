9726e1b065badf380b2e32dde574a3249da0ffcd
weikeyan <270022748@qq.com>
2018-03-15 10:50:11 +0800

    Commit

---
+++ ALGExe/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ALGExe
+//
+//  Created by weikeyan on 2018/3/15.
+//  Copyright © 2018年 weikeyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
